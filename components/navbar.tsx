c8b2053ea2c07d824b3df2b1e1f261da8024d9bb
Vulnet Sejfulai <timewebsorder@gmail.com>
2024-04-10 18:30:31 +0200

    navchanges

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,20 +3,20 @@
 import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { CiLogin } from "react-icons/ci";
-
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <>
-      <nav className='w-full flex justify-evenly max-[570px]:justify-between max-[570px]:p-5 items-center h-[70px] border-b-2 bg-transparent'>
+      <nav className='w-full flex justify-evenly max-[980px]:justify-between p-5 items-center h-[70px] border-b-2 bg-transparent'>
         <div className='text-2xl font-bold text-[#23395d]'>
           <a href="/">
             Time Energy
           </a>
         </div>
-        <div className='space-x-16 max-sm:space-x-5 text-base font-semibold max-[570px]:hidden'>
+        <div className='space-x-16 max-sm:space-x-5 text-base font-semibold max-[980px]:hidden'>
           <a href='/' className='hover:text-[#53637b]' onMouseEnter={() => setIsHovered(true)}>
             Chi siamo
           </a>
@@ -30,11 +30,20 @@ const Navbar = () => {
             Tariffe Telefoniche
           </a>
         </div>
-        <Button
-          className='relative hover:text-[#53637b]'
-          variant="outline">
-            Accedi <CiLogin size="18"/>
-        </Button>
+        <div className='flex'>
+            <Button
+            className='relative hover:text-[#53637b]'
+            variant="outline">
+                Accedi <CiLogin size="18"/>
+            </Button>
+        </div>
+        <div className='hidden max-[980px]:flex'>
+            <Button
+            className='hover:text-[#53637b]'
+            variant="outline">
+                <GiHamburgerMenu />
+            </Button>
+        </div>
       </nav>
       <div className={`absolute w-full flex justify-center bg-white transition-opacity duration-500 ease-in-out ${isHovered ? 'opacity-100' : 'opacity-0'}`} onMouseLeave={() => setIsHovered(false)}>
         {isHovered && "f"}
