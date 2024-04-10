f5cb9569a72e695c20cfaf2d4cab058812fead18
Vulnet Sejfulai <timewebsorder@gmail.com>
2024-04-10 23:23:17 +0200

    new db plus changes

--- components/dropdown-prod.tsx
+++ components/dropdown-prod.tsx
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const ProdDrop = () => {
+interface Category {
+    categorys: keyof LuceGasData;
+}
+
+interface LuceGasData {
+    id: number;
+    luce_gas: string;
+    internetcasa: string;
+    tariffetele: string;
+}
+
+const ProdDrop = ({ categorys }: Category) => {
    
-  const handleCategoryClick = (category: string) => {
-    localStorage.setItem('selectedCategory', category);
-  };
+    const [data, setData] = useState<LuceGasData[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("/api/luce_gas");
+            const data = await response.json();
+            setData(data);
+        };
 
-  return (
-    <div className='w-full flex justify-center'>
-        <div className='w-[50%] flex justify-center flex-wrap p-5 max-[1150px]:w-full max-[570px]:hidden'>
-            <div className='w-full justify-start flex text-gl font-medium h-[20px] pl-8 pb-10 pt-5 text-gray-500'>
-                Categories
-            </div>
-            <div className='w-full justify-start flex flex-wrap pl-8 h-[150px]'>
-                {/*data.categories.map((category, index) => (
-                    <div key={index} className="w-[33%] text-2xl font-bold">
-                        <a
-                            href={"/products/" + category.toLowerCase().replace(/\s+/g, '-')}
-                            className='hover:underline'
-                            onClick={() => handleCategoryClick(category)}
-                        >
-                            {category}
-                        </a>
-                    </div>
-                ))*/}
+        fetchData();
+    }, []);
+
+    const filteredData = data.filter((item) => item[categorys]);
+
+    const categoryNames: { [key in keyof LuceGasData]: string } = {
+        id: "",
+        tariffetele: "Tariffe Telefoniche",
+        luce_gas: "Luce e Gas",
+        internetcasa: "Internet Casa",
+    };
+
+    return (
+        <div className='w-full flex justify-center'>
+            <div className='w-[50%] flex justify-center flex-wrap p-5 max-[1150px]:w-full max-[570px]:hidden'>
+                <div className='w-full justify-start flex text-gl font-medium h-[20px] pl-8 pb-10 pt-5 text-gray-500'>
+                    {categoryNames[categorys]}
+                </div>
+                <div className='w-full justify-start flex flex-wrap pl-8 h-[150px]'>
+                    {filteredData.map((category, index) => (
+                        <div key={index} className="w-[33%] text-xl font-bold pr-5">
+                            <a className='hover:underline cursor-pointer'>
+                                {category[categorys]}
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default ProdDrop;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,55 +1,63 @@
 'use client'
 
-import React, { useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { CiLogin } from "react-icons/ci";
-import { GiHamburgerMenu } from "react-icons/gi";
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { CiLogin } from 'react-icons/ci';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import ProdDrop from '@/components/dropdown-prod';
 
 const Navbar = () => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHoveredLuceGas, setisHoveredLuceGas] = useState(false);
+  const [isHoveredInterCasa, setisHoveredInterCasa] = useState(false);
+  const [isHoveredTarifTel, setisHoveredTarifTel] = useState(false);
 
   return (
     <>
       <nav className='w-full flex justify-evenly max-[980px]:justify-between p-5 items-center h-[70px] border-b-2 bg-transparent'>
         <div className='text-2xl font-bold text-[#23395d]'>
-          <a href="/">
-            Time Energy
-          </a>
+          <a href='/'>Time Energy</a>
         </div>
         <div className='space-x-16 max-sm:space-x-5 text-base font-semibold max-[980px]:hidden'>
-          <a href='/' className='hover:text-[#53637b]' onMouseEnter={() => setIsHovered(true)}>
+          <a href='/' className='hover:text-[#53637b]' onMouseEnter={() => (setisHoveredLuceGas(false), setisHoveredInterCasa(false), setisHoveredTarifTel(false))}>
             Chi siamo
           </a>
-          <a href='' className='hover:text-[#53637b]' onMouseEnter={() => setIsHovered(true)}>
+          <a
+            href='/'
+            className='hover:text-[#53637b]'
+            onMouseEnter={() => (setisHoveredLuceGas(true), setisHoveredInterCasa(false), setisHoveredTarifTel(false))}>
             Luce e Gas
           </a>
-          <a href='/' className='hover:text-[#53637b]' onMouseEnter={() => setIsHovered(true)}>
-            Internet Casa 
+          <a
+            href='/'
+            className='hover:text-[#53637b]'
+            onMouseEnter={() => (setisHoveredInterCasa(true), setisHoveredTarifTel(false), setisHoveredLuceGas(false))}>
+            Internet Casa
           </a>
-          <a href='/' className='hover:text-[#53637b]' onMouseEnter={() => setIsHovered(true)}>
+          <a
+            href='/'
+            className='hover:text-[#53637b]'
+            onMouseEnter={() => (setisHoveredTarifTel(true), setisHoveredInterCasa(false), setisHoveredLuceGas(false))}>
             Tariffe Telefoniche
           </a>
         </div>
         <div className='flex'>
-            <Button
-            className='relative hover:text-[#53637b]'
-            variant="outline">
-                Accedi <CiLogin size="18"/>
-            </Button>
+          <Button className='relative hover:text-[#53637b]' variant='outline'>
+            Accedi <CiLogin size='18' />
+          </Button>
         </div>
         <div className='hidden max-[980px]:flex'>
-            <Button
-            className='hover:text-[#53637b]'
-            variant="outline">
-                <GiHamburgerMenu />
-            </Button>
+          <Button className='hover:text-[#53637b]' variant='outline'>
+            <GiHamburgerMenu />
+          </Button>
         </div>
       </nav>
-      <div className={`absolute w-full flex justify-center bg-white transition-opacity duration-500 ease-in-out ${isHovered ? 'opacity-100' : 'opacity-0'}`} onMouseLeave={() => setIsHovered(false)}>
-        {isHovered && "f"}
+      <div className={`absolute w-full flex justify-center bg-white transition-opacity duration-500 ease-in-out ${isHoveredLuceGas || isHoveredInterCasa || isHoveredTarifTel ? 'opacity-100' : 'opacity-0'}`} onMouseLeave={() => (setisHoveredLuceGas(false), setisHoveredInterCasa(false), setisHoveredTarifTel(false))}>
+        {isHoveredLuceGas && <ProdDrop categorys='luce_gas' />}
+        {isHoveredInterCasa && <ProdDrop categorys='internetcasa' />}
+        {isHoveredTarifTel && <ProdDrop categorys='tariffetele' />}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
