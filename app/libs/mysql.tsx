0edf06a2b6f26ae6aaf7c491b509b7a8c5b7e271
Vulnet Sejfulai <timewebsorder@gmail.com>
2024-04-11 18:52:45 +0200

    try3

--- app/libs/mysql.tsx
+++ app/libs/mysql.tsx
@@ -1,24 +1,11 @@
-import mysql from 'mysql2/promise';
-import { Pool } from 'mysql2/promise';
+import mysql from 'mysql2/promise'
 
-let pool: Pool;
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_SCHEMA,
+    waitForConnections: true
+})
 
-if (process.env.DB_SOURCE === 'local') {
-    pool = mysql.createPool({
-        host: process.env.LOCAL_DB_HOST,
-        user: process.env.LOCAL_DB_USER,
-        password: process.env.LOCAL_DB_PASS,
-        database: process.env.LOCAL_DB_SCHEMA,
-        waitForConnections: true
-    });
-} else {
-    pool = mysql.createPool({
-        host: process.env.ONLINE_DB_HOST,
-        user: process.env.ONLINE_DB_USER,
-        password: process.env.ONLINE_DB_PASS,
-        database: process.env.ONLINE_DB_SCHEMA,
-        waitForConnections: true
-    });
-}
-
-export default pool;
+export default pool
